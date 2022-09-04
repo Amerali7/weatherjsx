a33514e871250a1d8f8173d253ea464b261066c3
ABBAS MERALI <abbas_merali1@hotmail.com>
2022-09-04 16:23:34 +0100

    complete

--- README.md
+++ README.md
@@ -68,3 +68,97 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+import React, { useState } from "react";
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  const [data, setData] = useState([]);
+  const [dataf, setDataF] = useState([]);
+  const [location, setLocation] = useState('');
+  const url = `https://api.openweathermap.org/data/2.5/weather/?q=${location}&units=metric&APPID=d0c125cc52274ddb6c74c1243946e368`
+  const urlF =`https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=d0c125cc52274ddb6c74c1243946e368&cnt=5`  
+  const temp1 = 1;
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        //console.log(response.data)
+      })
+
+      axios.get(urlF).then((response) => {
+        setDataF(response.data)
+        console.log(response.data)
+        console.log(response.data.list[0].main.temp)
+    
+      })
+
+      setLocation('')
+      console.log(location);
+      
+    }
+  } 
+
+  return (
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='     Enter Location'
+          type="text"
+          className="enters" />
+      </div>
+
+
+      <div className="container">
+
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+
+
+<table>
+  <tr>
+    <td>{temp1}</td>
+  </tr>
+</table>
+
+        {
+        data.name !== undefined &&
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        }
+      </div>
+
+
+
+    </div>
+  );
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,83 +1,164 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import './App.css';
 import axios from 'axios';
 
+const api = {
+  key: "d0c125cc52274ddb6c74c1243946e368",
+  base: "https://api.openweathermap.org/data/2.5/",
+  icon: "http://openweathermap.org/img/w/"
+}
 function App() {
-  const [data, setData] = useState([]);
-  const [dataF, setDataF] = useState([]);
-  const [location, setLocation] = useState('');
-  const url = `https://api.openweathermap.org/data/2.5/weather/?q=${location}&units=metric&APPID=d0c125cc52274ddb6c74c1243946e368`
-  const urlF = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=d0c125cc52274ddb6c74c1243946e368&cnt=5`  
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+  const [forecast, setForecast] = useState({});
+ 
 
-  const searchLocation = (event) => {
-    if (event.key === 'Enter') {
-      axios.get(url).then((response) => {
-        setData(response.data)
-        console.log(response.data)
-      })
-      axios.get(urlF).then((response) => {
-        setDataF(response.data)
-        console.log(response.data)
-      })
 
-      setLocation('')
-      console.log(location);
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+          
+        });
+        if (evt.key === "Enter") {
+          fetch(`${api.base}forecast?q=${query}&units=metric&APPID=${api.key}&cnt=5`)
+            .then(res => res.json())
+            .then(result => {
+              setForecast(result);
+              console.log(result);
+            });
+        }
     }
-  } 
+  }
+
+  const getimg = (imgid) =>{
+
+    return "<img src=http://openweathermap.org/img/wn/"+imgid+"@2x.png>"
+  }
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+  const dayofweek = (weekday, i ) => {
+    let da = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let checker = weekday.getDay()+i;
+    let value = checker - 6;
+    if (checker > 6){
+      checker = value-1;
+    }
+    let dayi = da[checker];
+    return dayi
+  }
+
+
 
   return (
-    <div className="app">
-
-
-      <div className="search">
-        <input
-          value={location}
-          onChange={event => setLocation(event.target.value)}
-          onKeyPress={searchLocation}
-          placeholder='     Enter Location'
-          type="text"
-          className="enters" />
-      </div>
-
-
-      <div className="container">
-
-        <div className="top">
-          <div className="location">
-            <p>{data.name}</p>
-          </div>
-          <div className="temp">
-            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
-          </div>
-          <div className="description">
-            {data.weather ? <p>{data.weather[0].main}</p> : null}
-          </div>
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
         </div>
-
-
-        
-
-        {data.name !== undefined &&
-          <div className="bottom">
-            <div className="feels">
-              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
-              <p>Feels Like</p>
-            </div>
-            <div className="humidity">
-              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
-              <p>Humidity</p>
-            </div>
-            <div className="wind">
-              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
-              <p>Wind Speed</p>
-            </div>
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
           </div>
-        }
-      </div>
+          <div className="weather-box">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°c
+            </div>
+            <div className="weather">{weather.weather[0].main}</div>
+          </div>
 
+            <div className="forecast">
+            
+              <div className="temps">
+                <div className="forbox">
+                <p>
+                {dayofweek(new Date(),1)}
+                </p>
+                  <h1>
+                    {Math.round(forecast.list[0].main.temp)}°c
+                  </h1>
+                  <p>{forecast.list[0].weather[0].main}</p>
+                  <p> <img src = {`http://openweathermap.org/img/w/${forecast.list[0].weather[0].icon}.png`}/></p>
+                </div>
 
+                <div className="forbox">
+                  <p>{dayofweek(new Date(),2)}</p>
+                  <h1>{Math.round(forecast.list[1].main.temp)}°c</h1>
+                  <p>{forecast.list[1].weather[0].main}</p>
+                  <p><img src = {`http://openweathermap.org/img/w/${forecast.list[1].weather[0].icon}.png`}/></p>
+                </div>
+                <div className="forbox">
+                <p>{dayofweek(new Date(),3)}</p>
+                  <h1>{Math.round(forecast.list[2].main.temp)}°c</h1>
+                  <p>{forecast.list[2].weather[0].main}</p>
+                  <p><img src = {`http://openweathermap.org/img/w/${forecast.list[2].weather[0].icon}.png`}/></p>
+                </div>
+                <div className="forbox">
+                <p>{dayofweek(new Date(),4)}</p>
+                  <h1>{Math.round(forecast.list[3].main.temp)}°c</h1>
+                  <p>{forecast.list[3].weather[0].main}</p>
+                  <p><img src = {`http://openweathermap.org/img/w/${forecast.list[3].weather[0].icon}.png`}/></p>
+                </div>
+                <div className="forbox">
+                <p>{dayofweek(new Date(),5)}</p>
+                  <h1>{Math.round(forecast.list[4].main.temp)}°c</h1>
+                  <p>{forecast.list[4].weather[0].main}</p>
+                  <p><img src = {`http://openweathermap.org/img/w/${forecast.list[4].weather[0].icon}.png`}/></p>
+                </div>
+              </div>
+               </div>
+               
+        <div className="add-info">
+                  <div className="add-data">
+                  <div className="forbox">
+                    <h2>Feels Like</h2>
+                    <h1>{weather.main.feels_like}°c</h1>
+                  </div>
+                  <div className="forbox">
+                  <h2>Humidity</h2>
+                  <h1>{weather.main.humidity} %</h1>
+                  </div>
+                  <div className="forbox">
+                    <h2>Wind Speed</h2>
+                    <h1>{weather.wind.speed} Mph</h1>
+                  </div>
+                    
+                  </div>
 
+              </div>
+        </div>
+        
+        ) : ('')}
+
+      </main>
+     
     </div>
+   
   );
 }
 
